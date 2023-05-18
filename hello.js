11ebb34fc36d4bb2cf4e01075aacff33884333c3
Rizzel <rizzel_nolasco17@yahoo.com>
2023-05-18 03:52:58 +0000

    console.log and return hello.js and check if number is even even.js

--- hello.js
+++ hello.js
@@ -5,3 +5,16 @@ const sayHello = function(name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+// console.log
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+// return
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
